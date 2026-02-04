09a177cbbd14f938c6ec416a60886574eebe7896
phillipwachsmann <phillip.wachsmann@bwz-rappi.ch>
2026-02-04 11:11:52 +0100

    changed to the right prettier extension, styling changes to make tab-bar bigger

--- bwzRappiIonicApp/src/views/TabsPage.vue
+++ bwzRappiIonicApp/src/views/TabsPage.vue
@@ -2,23 +2,23 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom" size="large">
-        <ion-tab-button tab="tab1" href="/tabs/tab1" size="large">
+      <ion-tab-bar slot="bottom" class="tab-bar">
+        <ion-tab-button tab="tab1" href="/tabs/tab1" class="tab-button">
           <ion-icon aria-hidden="true" :icon="triangle" />
           <ion-label>Play </ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2" size="large">
+        <ion-tab-button tab="tab2" href="/tabs/tab2" class="tab-button">
           <ion-icon aria-hidden="true" :icon="ellipse" />
           <ion-label>History </ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3" size="large">
+        <ion-tab-button tab="tab3" href="/tabs/tab3" class="tab-button">
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Help </ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab4" href="/tabs/tab4" size="large">
+        <ion-tab-button tab="tab4" href="/tabs/tab4" class="tab-button">
           <ion-icon aria-hidden="true" :icon="square" />
           <ion-label>Settings </ion-label>
         </ion-tab-button>
